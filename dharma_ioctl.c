45236ce24b3c6bbe9e895f3fe3511c974d902644
Manuel Coppotelli <manuelcop@gmail.com>
2016-07-08 16:50:19 +0200

    ioctl cli
    ioctl command line interface to set device parameters

--- dharma_ioctl.c
+++ dharma_ioctl.c
@@ -63,9 +63,79 @@ void reset_blocking_flag(int fd)
 
 int main(int argc, char const *argv[])
 {
-	char *file_name = "/dev/dharma";
+	if (argc < 3)
+		goto help;
 
-	/* Set the flags */
+	int minor = argv[1];
+
+	int mode_stream = 0;
+	int mode_packet = 0;
+	int blocking = 0;
+	int not_blocking = 0;
+
+	int n = 2;
+
+	while ( n < argc )
+	{
+		if ( !strncmp(argv[n], "-s", 2) || !strncmp(argv[n], "-S", 2) )
+		{
+			mode_stream = 1;
+		}
+
+		if ( !strncmp(argv[n], "-p", 2) || !strncmp(argv[n], "-P", 2) )
+		{
+			mode_packet = 1;
+		}
+
+		if ( !strncmp(argv[n], "-b", 2) || !strncmp(argv[n], "-B", 2) )
+		{
+			blocking = 1;
+		}
+
+		if ( !strncmp(argv[n], "-nb", 3) || !strncmp(argv[n], "-NB", 3) )
+		{
+			not_blocking = 1;
+		}
+
+		++n;
+	}
+
+	if (mode_stream && mode_packet)
+		goto help;
+
+	if (blocking && not_blocking)
+		goto help;
+
+	char *file_name = "/dev/dharma";
+	char new_file_name[strlen(file_name) + 1];
+	int fd;
+
+	strcpy(new_file_name, file_name);
+	strcat(new_file_name, minor);
+
+	fd = open(new_file_name, O_RDWR);
+
+	if (fd == -1)
+	{
+		perror("ioctl open error\n");
+		return 2;
+	}
+
+	if (mode_stream)
+		set_read_mode(fd, DHARMA_STREAM_MODE);
+
+	if (mode_packet)
+		set_read_mode(fd, DHARMA_PACKET_MODE);
+
+	if(blocking)
+		set_blocking_flag(fd);
+
+	if(not_blocking)
+		reset_blocking_flag(fd);
 
 	return 0;
+
+help:
+	fprintf(stderr, "Usage: %s minor [-s | -p] [-b | -nb]\n", argv[0]);
+	return 1;
 }
