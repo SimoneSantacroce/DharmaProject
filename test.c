ed944db12b03c4cd0e44cc214ab749e35e1a67b6
SaraVeterini <saraveterini@gmail.com>
2016-07-20 13:11:33 +0200

    new test

--- test.c
+++ test.c
@@ -44,23 +44,54 @@ int main(void)
     
     char data[128];
  
-    if(read(filedesc, data, 128) < 0)
+    /*if(read(filedesc, data, 128) < 0)
 		printf("An error occurred in the read.\n");
 		
-	printf("Read data:\n%s", data);
+	printf("Read data:\n%s", data);*/
 
 	/* TEST BY ROB */
 	printf("Testing now blocking mode on read stream...\n");
 	if( ioctl(filedesc, DHARMA_SET_NONBLOCKING, 0) == 0)
 		printf("Set NON BLOCKING mode\n");
 
-	int res;
+	/*int res;
 	res = read(filedesc, data, 128);
 	if( res < 0 )
 		printf("An error occurred in the read.");
 	if( res == 0 )
-		printf("res is equal to 0 => (probably) we called read in non blocking mode.");
+		printf("res is equal to 0 => (probably) we called read in non blocking mode.");*/
 
+	/* TEST BY SARA */
+	printf("Testing now blocking mode on read packet...\n");
+	if( ioctl(filedesc, DHARMA_SET_PACKET_MODE, 0) == 0)
+		printf("Set PACKET mode\n");
+		
+	int res2;
+	res2 = read(filedesc, data, 128);
+	if( res2 < 0 )
+		printf("An error occurred in the read.");
+	if( res2 == 0 )
+		printf("res is equal to 0 => (probably) we called read in non blocking mode.");
+	
+	printf("Read data:\n%s\n", data);
+	
+	int res3;
+	res3 = read(filedesc, (char *)(&data[res2]), 128);
+	if( res3 < 0 )
+		printf("An error occurred in the read.");
+	if( res3 == 0 )
+		printf("res is equal to 0 => (probably) we called read in non blocking mode.");
+	
+	printf("Read data:\n%s\n", data);
+	
+	int res4;
+	res4 = read(filedesc, (char *)(&data[res3+res2]), 128);
+	if( res4 < 0 )
+		printf("An error occurred in the read.");
+	if( res4 == 0 )
+		printf("res is equal to 0 => (probably) we called read in non blocking mode.");
+	
+	printf("Read data:\n%s\n", data);
 	/* TEST BY FABIO
 	printf("Testing now blocking mode on read stream...\n");
 	if (fd_set_blocking(filedesc, 0))
